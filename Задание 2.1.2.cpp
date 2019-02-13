ccd711a4578b0a49d8d3624ee17f7ff930467138
EVOgeek618 <37446747+EVOgeek618@users.noreply.github.com>
2019-02-13 15:30:38 +0300

    Create Задание 2.1.2.cpp

---
+++ Задание 2.1.2.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"                                                            
+#include "std_msgs/Int32.h"                                                     
+#include <iostream>                                                             
+using namespace std;                                                            
+void fun(const std_msgs::Int32 a){                                              
+        cout<<"/subscriber: data: "<<a.data;                                    
+return;                                                                         
+}                                                                               
+                                                                                
+int main (int argc,char **argv){                                                
+        ros::init(argc,argv,"subscriber");                                      
+        ros::NodeHandle n;                                                      
+        ros::Subscriber sub=n.subscribe("/topic",1000,fun);                     
+        ros::spin();                                                            
+        return 0;                                                               
+}                                                                               
+    
